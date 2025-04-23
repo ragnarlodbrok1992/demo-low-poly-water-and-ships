ca81d92939029411b23b1a2eb7d81c2fb41bb719
ragnarlodbrok1992 <maraus92@gmail.com>
2025-04-23 18:14:11 +0200

    Added helper functions

--- src/render_directx3d12.cpp
+++ src/render_directx3d12.cpp
@@ -7,3 +7,27 @@ const char* Vertex_to_string(const Vertex& vert) {
   return buffer;
 };
 
+inline void ThrowIfFailed(HRESULT hr) {
+  if (FAILED(hr)) {
+    throw std::runtime_error("Failed HRESULT");
+  }
+}
+
+void GetHardwareAdapter(IDXGIFactory4* pFactory, IDXGIAdapter1** ppAdapter) {
+  *ppAdapter = nullptr;
+  for (UINT adapterIndex = 0; ; ++adapterIndex) {
+      IDXGIAdapter1* pAdapter = nullptr;
+      if (DXGI_ERROR_NOT_FOUND == pFactory->EnumAdapters1(adapterIndex, &pAdapter)) {
+          // No more adapters to enumerate.
+          break;
+      } 
+
+      // Check to see if the adapter supports Direct3D 12, but don't create the
+      // actual device yet.
+      if (SUCCEEDED(D3D12CreateDevice(pAdapter, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr))) {
+          *ppAdapter = pAdapter;
+          return;
+      }
+      pAdapter->Release();
+  }
+}
--- src/render_directx3d12.hpp
+++ src/render_directx3d12.hpp
@@ -1,15 +1,30 @@
 #ifndef _H_RENDER_DIRECTX3D12
 #define _H_RENDER_DIRECTX3D12
 
+#define _WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 #include <DirectXMath.h>
+#include <dxgi1_6.h>
 #include <cstdio>
+#include <stdexcept>
 
+#include "directx/d3dcommon.h"
+#include "directx/d3d12.h"
+
+// Vertex stuff
 struct Vertex {
   DirectX::XMFLOAT3 position;
   DirectX::XMFLOAT4 color;
 };
 
 const char* Vertex_to_string(const Vertex& vert);
+
+// Render stuff
+
+
+// Helper functions
+inline void ThrowIfFailed(HRESULT hr); 
+void GetHardwareAdapter(IDXGIFactory4* pFactory, IDXGIAdapter1** ppAdapter);
 
 #endif /* _H_RENDER_DIRECTX3D12 */
 
